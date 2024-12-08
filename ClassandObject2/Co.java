6c88557cd67d71ae89ed372e82d1bbd4532bbd37
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-12-08 12:48:13 +0530

    Class and Object

---
+++ ClassandObject2/Co.java
@@ -0,0 +1,36 @@
+package ClassandObject2;
+
+class student {
+    private String name;
+    private int age;
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setAge(int a) {
+        age = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Co {
+    public static void main(String[] args) {
+        student obj = new student();
+
+        obj.setName("Abhey");
+        obj.setAge(30);
+
+        System.out.println(obj.getName());
+        ;
+        System.out.println(obj.getAge());
+        ;
+    }
+}
