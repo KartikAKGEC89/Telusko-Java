5f91b063fb2ae22da5797de553e541a7aaacd720
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-12-08 11:59:13 +0530

    String and StringBuffer

---
+++ String/StringBufferTest.java
@@ -0,0 +1,15 @@
+package String;
+
+public class StringBufferTest {
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("Abhey");
+        System.out.println(str);
+
+        str.insert(5, " Java");
+
+        System.out.println(str);
+    }
+}
+
+// Class.foName() -> This is method to load the class.
+// Static Keyword use to define one particular value or one data.
---
+++ String/StringTest.java
@@ -0,0 +1,14 @@
+package String;
+
+public class StringTest {
+    public static void main(String[] args) {
+        // String is not datatype in java its basically a class.
+
+        String name = new String("Kartik");
+        System.out.println(name);
+
+        System.out.println("Hello" + " " + name);
+
+        System.out.println(name.charAt(3));
+    }
+}
