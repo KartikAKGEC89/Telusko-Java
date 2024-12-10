9bbc637317ac00b0cba266f92081a3878f362898
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-12-10 14:09:37 +0530

    final done

---
+++ FinalKeyword/FinalKeyword.java
@@ -0,0 +1,10 @@
+package FinalKeyword;
+
+// Final keyword in java are used in :- variable, method, class
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        final int num = 1;
+        System.out.println(num);
+    }
+}
