91c66a5ae0c427f2d033a6e5737cfd3be72dc5d3
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-12-10 14:10:42 +0530

    final done

--- FinalKeyword/FinalKeyword.java
+++ FinalKeyword/FinalKeyword.java
@@ -2,9 +2,25 @@ package FinalKeyword;
 
 // Final keyword in java are used in :- variable, method, class
 
+// Final stop extends for class
+final class A {
+    // final stop override for method
+    final public void show() {
+        System.out.println("hello");
+    }
+}
+
+// class B extends A {
+// // public void show() {
+// // System.out.println("Not done");
+// // }
+// }
+
 public class FinalKeyword {
     public static void main(String[] args) {
+        // final stop reinitialize here
         final int num = 1;
+        // num = 2;
         System.out.println(num);
     }
 }
