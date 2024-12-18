736bda5120d77291bec589712d4dba76e06f65f3
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-12-18 12:30:02 +0530

    Interface

--- Interfaces/WhatisInterface.java
+++ Interfaces/WhatisInterface.java
@@ -46,3 +46,11 @@ public class WhatisInterface {
 // class - class -> extends
 // class - interface -> implements
 // interface - interface -> extends
+
+// Types of Interface:-
+// 1. Normal interface
+// - an interface having two or more methods
+// 2. Functional interface (SAM)
+// - SAM => Single Abstract Method interface
+// 3. Marker interface
+// - an interface that as no methods (blank interface)
